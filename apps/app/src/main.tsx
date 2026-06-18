9440c670b88b1088688087d328c449e321b826ab
be646 <be646@users.noreply.github.com>
2026-06-18 18:28:03 +0200

    Fix Clerk custom domain and auth hooks

--- apps/app/src/main.tsx
+++ apps/app/src/main.tsx
@@ -57,6 +57,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ClerkProvider
       publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}
+      domain={import.meta.env.VITE_CLERK_FRONTEND_API_URL}
       appearance={{ elements: { badge: "hidden", logoBox: "hidden" } }}
     >
       <QueryClientProvider client={queryClient}>
